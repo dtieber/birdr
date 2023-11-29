101993d4f471c1973219c7d66de6d97dd49a2f42
Daniel Tieber <daniel.tieber@dynatrace.com>
2023-11-29 15:50:03 +0100

    feat(editor): use log level specified in config

--- packages/editor/src/logger/logging.configuration.ts
+++ packages/editor/src/logger/logging.configuration.ts
@@ -1,5 +1,6 @@
 import type { PinoLoggerOptions } from 'fastify/types/logger'
 
+import { config } from '../config'
 import { logLevelFormatter, timestampFormatter } from './logging.formatter'
 
 const loggerFormatters = {
@@ -7,7 +8,7 @@ const loggerFormatters = {
 }
 
 export const loggerConfig: PinoLoggerOptions = {
-  level: 'info',
+  level: config.LOG_LEVEL,
   messageKey: 'message',
   formatters: loggerFormatters,
   redact: {
