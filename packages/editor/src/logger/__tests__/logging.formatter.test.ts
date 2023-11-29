b75cb9167b04fee8c1544124bf0c8051490ec038
Daniel Tieber <daniel.tieber@dynatrace.com>
2023-11-29 15:10:30 +0100

    feat(editor): add custom logging formatter for log levels

--- packages/editor/src/logger/__tests__/logging.formatter.test.ts
+++ packages/editor/src/logger/__tests__/logging.formatter.test.ts
@@ -1,8 +1,16 @@
 import { describe, expect, it, jest } from '@jest/globals'
 
-import { timestampFormatter } from '../logging.formatter'
+import { logLevelFormatter, timestampFormatter } from '../logging.formatter'
 
 describe('logging.formatter', () => {
+  it('logLevelFormatter uses label for level', () => {
+    const formatted = logLevelFormatter('INFO', 0)
+
+    expect(formatted).toEqual({
+      level: 'INFO',
+    })
+  })
+
   it('timestampFormatter creates iso string', () => {
     Date.now = jest.fn(() => new Date(Date.UTC(2222, 0, 1)).valueOf())
 
