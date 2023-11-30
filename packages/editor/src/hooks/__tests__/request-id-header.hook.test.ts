35cec58a1982055f931947658532910ba3d03804
Daniel Tieber <daniel.tieber@dynatrace.com>
2023-11-30 14:59:25 +0100

    feat(editor): add hook to add `x-request-id` header to response

---
+++ packages/editor/src/hooks/__tests__/request-id-header.hook.test.ts
@@ -0,0 +1,29 @@
+import { afterAll, beforeAll, describe, expect, it } from '@jest/globals'
+import type { FastifyInstance } from 'fastify'
+import Fastify from 'fastify'
+
+import { requestIdHeader } from '../request-id-header.hook'
+
+describe('get-origins-route', () => {
+
+  let application: FastifyInstance
+
+  beforeAll(async() => {
+    application = Fastify()
+    await application.register(requestIdHeader)
+  })
+
+  afterAll(async() => {
+    await application.close()
+  })
+
+  it('exposes a request id in the header', async () => {
+    const response = await application.inject({
+      method: 'GET',
+      url: '/',
+    })
+
+    expect(response.headers['x-request-id']).not.toBeUndefined()
+  })
+
+})
