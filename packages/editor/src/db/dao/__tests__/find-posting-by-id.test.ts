9c8fc50a3564ad1785b95495e35351694c99f4f6
Daniel Tieber <daniel.tieber@dynatrace.com>
2023-12-20 09:18:50 +0100

    test(editor): add integration test for happy path of find-posting-by-id

--- packages/editor/src/db/dao/__tests__/find-posting-by-id.test.ts
+++ packages/editor/src/db/dao/__tests__/find-posting-by-id.test.ts
@@ -1,8 +1,11 @@
 import { afterAll, describe, expect, it } from '@jest/globals'
 import { pino } from 'pino'
 
+import { isError } from '../../../errors/is-error.util'
 import { loggerConfig } from '../../../logger/logging.configuration'
 import { dbInstance } from '../../client'
+import { addPosting } from '../add-posting'
+import { addUser } from '../add-user'
 import { findPostingById } from '../find-posting-by-id'
 
 describe('find-posting-by-id dao', () => {
@@ -12,10 +15,25 @@ describe('find-posting-by-id dao', () => {
     await dbInstance.destroy()
   })
 
-  it('returns error if posting cannot be found', async () => {
-    const foundPosting = await findPostingById(logger, 666)
+  it('returns posting', async () => {
+    const username = `my-username-${Date.now()}`
+    const user = await addUser(logger, username)
+    if(isError(user)) {
+      throw new Error('Failed to insert user')
+    }
+    const postingText = 'hello world'
+    const posting = await addPosting(logger, user.id, postingText)
+    if(isError(posting)) {
+      throw new Error('Failed to insert posting')
+    }
 
-    expect(foundPosting).toMatchObject({ message:'Posting with id 666 not found' })
+    const foundPosting = await findPostingById(logger, posting.id)
+
+    expect(foundPosting).toMatchObject({
+      text: postingText,
+      approved: false,
+      userid: user.id,
+    })
   })
 
 })
