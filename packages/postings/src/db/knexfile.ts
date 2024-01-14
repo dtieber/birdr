335ab27463cd48571792cf962292350f447234bd
Daniel Tieber <daniel.tieber@dynatrace.com>
2024-01-14 19:21:27 +0100

    feat(postings): use specific port for knex db config

--- packages/postings/src/db/knexfile.ts
+++ packages/postings/src/db/knexfile.ts
@@ -4,6 +4,8 @@ export const dbConfig: { [key: string]: Knex.Config } = {
   development: {
     client: 'postgresql',
     connection: {
+      host: 'localhost',
+      port: 5001,
       database: 'postings',
       user: 'myuser',
       password: 'mypassword',
