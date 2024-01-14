e9721e6d751006c196343c1160b9adbf8c628ae3
Daniel Tieber <daniel.tieber@dynatrace.com>
2024-01-14 17:38:29 +0100

    feat(users): add logging configuration for pino

---
+++ packages/users/src/logger/logging.configuration.ts
@@ -0,0 +1,22 @@
+import { logLevelFormatter, timestampFormatter } from '@birdr/shared'
+import type { PinoLoggerOptions } from 'fastify/types/logger'
+
+import { config } from '../config'
+
+const loggerFormatters = {
+  level: logLevelFormatter,
+}
+
+export const loggerConfig: PinoLoggerOptions = {
+  level: config.LOG_LEVEL,
+  messageKey: 'message',
+  formatters: loggerFormatters,
+  redact: {
+    paths: [
+      'hostname',
+      'pid',
+    ],
+    remove: true,
+  },
+  timestamp: timestampFormatter,
+}
