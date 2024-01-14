8d67a84d4eb320de3a3d9c01e87258855e439c74
Daniel Tieber <daniel.tieber@dynatrace.com>
2024-01-14 18:45:31 +0100

    feat(editor): add function to find user by id
    we'll use errors for both, business and technical errors. test is the negative case because we cannot add users yet, will adapt later.
    
    copied over from package `postings`

---
+++ packages/users/src/db/dao/__tests__/find-user-by-id.itest.ts
@@ -0,0 +1,32 @@
+import { isError } from '@birdr/shared'
+import { afterAll, describe, expect, it } from '@jest/globals'
+import { pino } from 'pino'
+
+import { loggerConfig } from '../../../logger/logging.configuration'
+import { dbInstance } from '../../client'
+import { addUser } from '../add-user'
+import { findUserById } from '../find-user-by-id'
+
+describe('find-user-by-id dao', () => {
+  const logger = pino(loggerConfig)
+
+  afterAll(async () => {
+    await dbInstance.destroy()
+  })
+
+  it('returns user', async () => {
+    const username = `my-username-${Date.now()}`
+    const user = await addUser(logger, username)
+    if(isError(user)) {
+      throw new Error('Failed to insert user')
+    }
+
+    const foundUser = await findUserById(logger, user.id)
+
+    expect(foundUser).toMatchObject({
+      id: user.id,
+      username,
+    })
+  })
+
+})
---
+++ packages/users/src/db/dao/find-user-by-id.ts
@@ -0,0 +1,30 @@
+import pino from 'pino'
+
+import { dbInstance } from '../client'
+
+import BaseLogger = pino.BaseLogger
+import type { User } from '../types/user'
+
+export async function findUserById(logger: BaseLogger, id: string): Promise<User | Error> {
+  try {
+    const maybeUser = await dbInstance.select<User>('*')
+      .from('user')
+      .where({
+        id,
+      })
+      .first()
+    if(!maybeUser) {
+      return new Error(`User with id ${id} not found`)
+    }
+    return {
+      id: maybeUser.id.toString(),
+      username: maybeUser.username,
+    }
+  } catch (err) {
+    logger.warn({
+      message: `DB error while finding user by id ${id}`,
+      err,
+    })
+    return err as Error
+  }
+}
