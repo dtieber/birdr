73ba9b3e2b18abfddd58eae34389bf5bf8b56940
Daniel Tieber <daniel.tieber@dynatrace.com>
2024-03-28 14:55:31 +0100

    feat(shared): expose Failure and type guard

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,4 +1,6 @@
 export * from './auth/auth-handler.decorator'
+export * from './errors/failure'
 export * from './errors/is-error.util'
+export * from './errors/is-failure.util'
 export * from './hooks/request-id-header.hook'
 export * from './logger/logging.formatter'
