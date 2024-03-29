4911abe86430443cd8c90e69f745dcd13bac26dd
Daniel Tieber <daniel.tieber@dynatrace.com>
2024-03-29 13:52:15 +0100

    feat(postings): use pino-pretty for local development

--- packages/postings/src/logger/logging.configuration.ts
+++ packages/postings/src/logger/logging.configuration.ts
@@ -19,4 +19,13 @@ export const loggerConfig: PinoLoggerOptions = {
     remove: true,
   },
   timestamp: timestampFormatter,
+  ...config.NODE_ENV === 'development' && {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        messageKey: 'message',
+        colorize: true,
+      },
+    },
+  },
 }
