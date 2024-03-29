d4746b41c782c78acb03ce89339444cedf0b62b5
Daniel Tieber <daniel.tieber@dynatrace.com>
2024-03-29 13:48:24 +0100

    feat(users): map NODE_ENV to config values

--- packages/users/src/config.ts
+++ packages/users/src/config.ts
@@ -7,5 +7,6 @@ export const config = {
   HOST: env.get('HOST').required().asString(),
   KAFKA_BROKER: env.get('KAFKA_BROKER').required().asString(),
   LOG_LEVEL: env.get('LOG_LEVEL').required().asString(),
+  NODE_ENV: env.get('NODE_ENV').required().asString(),
   PORT: env.get('PORT').required().asIntPositive(),
 }
