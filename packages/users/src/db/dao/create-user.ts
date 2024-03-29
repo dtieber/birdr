04a424ffa49920343aea1e4afb44be84630f2b0b
Daniel Tieber <daniel.tieber@dynatrace.com>
2024-03-29 08:57:56 +0100

    feat(users): return custom failure class instead of generic error

--- packages/users/src/db/dao/create-user.ts
+++ packages/users/src/db/dao/create-user.ts
@@ -1,19 +1,23 @@
 import pino from 'pino'
 import BaseLogger = pino.BaseLogger
 
+import { Failure } from '@birdr/shared'
+
 import { dbInstance } from '../client'
 import type { User } from '../types/user'
 import { readUserById } from './read-user-by-id'
 
-export async function createUser(logger: BaseLogger, username: string): Promise<User | Error> {
+export async function createUser(logger: BaseLogger, username: string): Promise<User | Failure> {
   try {
     const result = await dbInstance.insert<User>({ username }).returning('id').into('user')
     return readUserById(logger, result[0].id)
   } catch (err) {
+    const e = err as PgError
     logger.warn({
       message: `DB error while creating user ${username}`,
-      err,
+      e,
     })
-    return err as Error
+
+    return new Failure(e.message)
   }
 }
--- packages/users/src/db/dao/read-user-by-id.ts
+++ packages/users/src/db/dao/read-user-by-id.ts
@@ -3,9 +3,12 @@ import pino from 'pino'
 import { dbInstance } from '../client'
 
 import BaseLogger = pino.BaseLogger
+import { Failure } from '@birdr/shared'
+
+import { ErrorCodes } from '../../errors/error-codes'
 import type { User } from '../types/user'
 
-export async function readUserById(logger: BaseLogger, id: string): Promise<User | Error> {
+export async function readUserById(logger: BaseLogger, id: string): Promise<User | Failure> {
   try {
     const maybeUser = await dbInstance.select<User>('*')
       .from('user')
@@ -14,17 +17,18 @@ export async function readUserById(logger: BaseLogger, id: string): Promise<User
       })
       .first()
     if(!maybeUser) {
-      return new Error(`User with id ${id} not found`)
+      return new Failure(`User with id ${id} not found`, ErrorCodes.USER_NOT_FOUND)
     }
     return {
       id: maybeUser.id.toString(),
       username: maybeUser.username,
     }
   } catch (err) {
+    const e = err as Error
     logger.warn({
       message: `DB error while reading user by id ${id}`,
       err,
     })
-    return err as Error
+    return new Failure(e.message)
   }
 }
